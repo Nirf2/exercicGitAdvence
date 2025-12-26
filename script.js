2ee4feee457a6c41d7bf66f6b5de273ae27a8804
Kevin P <50870709+Nirfs@users.noreply.github.com>
2025-12-26 14:03:32 +0100

    C20 : changement du fond

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 
-const background_color = "white";
+const background_color = "blue";
 const border = "yellow";
 const text = "black";
 
